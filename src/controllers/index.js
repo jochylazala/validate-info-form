05fd980446d1f1024311664fe258b8160f6cabd4
jochylazala <62323712+jochylazala@users.noreply.github.com>
2021-06-22 12:05:51 -0400

    update

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -16,18 +16,42 @@ controller.in =  (req, res) => {
 			correo,
 			firstDate
 	};
-	let verify_email = newuser.correo;
-	newuser.firstDate = new Date();
-			
-	function Check_Data(typeEmail){
-		return typeEmail.correo === verify_email;
-	}
-	let finalCheck = userData.find(Check_Data)
-	
-	let first_date, second_date, milisecond,transcurrido, id;
-	let days = 0, missingDay = 0;
 
-	if(finalCheck === undefined){
+	class User{
+		constructor(emailTyped,date, milisecond, transcurrido, days, missingDay,hours,minutes,seconds,mili,second_date){
+			this.emailTyped = emailTyped;
+			this.date = date;
+			this.milisecond = milisecond;
+			this.transcurrido = transcurrido;
+			this.days = days;
+			this.missingDay = missingDay;
+			this.hours = hours;
+			this.minutes = minutes;
+			this.seconds = seconds;
+			this.mili = mili;
+			this.second_date = second_date;
+		}
+
+		get miliDay(){
+			return this.time();
+		}
+
+		time(){
+			return this.milisecond = this.hours * this.minutes * this.seconds * this.mili;
+		}
+	}
+	
+	let data = new User(newuser.correo, new Date(),0,0,0,0,24,60,60,1000, new Date());
+	newuser.firstDate = data.date; //that is for the date that will be sent to the database, the correct date
+	
+	function Check_Data(typeEmail){
+		return typeEmail.correo === data.emailTyped;
+	}
+	let foundData = userData.find(Check_Data);
+
+	let first_date;
+
+	if(foundData === undefined){
 		pool.query('INSERT INTO usuario set ?', [newuser]);
 		res.sendFile(path.join(__dirname,'../public/second.html'))
 
@@ -37,24 +61,22 @@ controller.in =  (req, res) => {
 
 	function update(){
 		insertData();
-		 if(finalCheck.correo == verify_email && days > 60 ){	
+		 if(foundData.correo == data.emailTyped && data.days > 60 ){	
 			pool.query('INSERT INTO usuario set ?', [newuser]);
 			res.sendFile(path.join(__dirname,'../public/second.html'))
 		
 		}else{
-			res.send("Debes de esperar para volverlo a intentar" + " " + missingDay + " " + "dias");
+			res.send("Debes de esperar para volverlo a intentar" + " " + data.missingDay + " " + "dias");
 
 		}
 
 	}
 
 	function insertData(){
-		first_date =   new Date(finalCheck.firstDate);
-		second_date = new Date()
-		milisecond = 24 * 60 * 60 * 1000;
-		transcurrido = Math.abs(first_date.getTime() - second_date.getTime());
-		days = Math.round(transcurrido / milisecond);
-		missingDay  = Math.abs(days - 60);
+		first_date =   new Date(foundData.firstDate);
+		data.transcurrido = Math.abs(first_date.getTime() - data.second_date.getTime());
+		data.days = Math.round(data.transcurrido / data.miliDay);
+		data.missingDay  = Math.abs(data.days - 60);
 	};
 
 	});
